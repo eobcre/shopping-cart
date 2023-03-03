2abdfd55167f3545daeb6554fe09b86d45192a17
eobcre <eobcre@gmail.com>
2023-03-03 13:31:10 -0500

    items displayed in map

--- app.js
+++ app.js
@@ -1,19 +1,94 @@
-let main = document.getElementById('main');
+const main = document.getElementById('main');
 
-let generateMain = () => {
-  return (main.innerHTML = `
-  <div class="item">
+// Item list
+const itemList = [
+  {
+    id: 0,
+    name: 'Apple',
+    price: 3.0,
+    img: './img/apple.svg',
+  },
+  {
+    id: 1,
+    name: 'Banana',
+    price: 3.0,
+    img: './img/banana.svg',
+  },
+  {
+    id: 2,
+    name: 'Brocolli',
+    price: 4.0,
+    img: './img/brocolli.svg',
+  },
+  {
+    id: 3,
+    name: 'Carrot',
+    price: 3.0,
+    img: './img/carrot.svg',
+  },
+  {
+    id: 4,
+    name: 'Corn',
+    price: 3.0,
+    img: './img/corn.svg',
+  },
+  {
+    id: 5,
+    name: 'Green Onion',
+    price: 2.0,
+    img: './img/green-onion.svg',
+  },
+  {
+    id: 6,
+    name: 'Lemon',
+    price: 2.0,
+    img: './img/lemon.svg',
+  },
+  {
+    id: 7,
+    name: 'Lettuce',
+    price: 4.0,
+    img: './img/lettuce.svg',
+  },
+  {
+    id: 8,
+    name: 'Onion',
+    price: 3.0,
+    img: './img/onion.svg',
+  },
+  {
+    id: 9,
+    name: 'Potato',
+    price: 3.0,
+    img: './img/potato.svg',
+  },
+  {
+    id: 10,
+    name: 'Radish',
+    price: 4.0,
+    img: './img/radish.svg',
+  },
+];
+
+const generateMain = () => {
+  return (main.innerHTML = itemList
+    .map((item) => {
+      const { id, name, price, img } = item;
+      return `
+    <div id=${id} class="item">
         <figure>
-          <img src="./img/apple.svg" alt="Icon" width="100" height="100" />
-          <figcaption>
-            <h2>Apple</h2>
-            <p>$3.00</p>
-          </figcaption>
+            <img src=${img} alt="Icon" width="100" height="100" />
+        <figcaption>
+            <h2>${name}</h2>
+            <p>$ ${price}</p>
+        </figcaption>
         </figure>
         <button class="cart-btn">Add To Cart</button>
         <a href="confirm.html"><button>Remove</button></a>
-      </div>
-  `);
+    </div>
+    `;
+    })
+    .join(''));
 };
 
 generateMain();
