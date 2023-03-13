1a810219668a5245b49dba4613ffb0dad068a608
eobcre <eobcre@gmail.com>
2023-03-13 14:24:14 -0400

    decrement function removed

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ generateMain();
 // increment
 let increment = (id) => {
   // check id exist or not === id selected
-  let search = storage.find((check) => check.id === id);
+  let search = storage.find((x) => x.id === id);
   // if the item does not exist then push
   if (search === undefined) {
     storage.push({
@@ -51,43 +51,20 @@ let increment = (id) => {
   localStorage.setItem('data', JSON.stringify(storage));
 };
 
-// decrement
-let decrement = (id) => {
-  // check id exist or not === id selected
-  let search = storage.find((x) => x.id === id);
-
-  // if search is undefined do nothing
-  if (search === undefined) return;
-  // stop going under 1
-  else if (search.item === 0) return;
-  else {
-    search.item -= 1;
-  }
-
-  update(id);
-
-  // filter the ones that are item: number but not 0
-  storage = storage.filter((x) => x.item !== 0);
-
-  // local storage settings
-  // save the data in local storage
-  localStorage.setItem('data', JSON.stringify(storage));
-};
-
 // updates on console
-const update = (id) => {
-  const search = storage.find((check) => check.id === id);
+let update = (id) => {
+  storage.find((x) => x.id === id);
   updateCal();
 };
 
-// calculate and update on cart
-const updateCal = () => {
+// calculate and show on cart
+let updateCal = () => {
   const showCart = document.getElementById('cartAmount');
   showCart.innerHTML = storage
-    .map((check) => check.item)
+    .map((x) => x.item)
     .reduce((acc, next) => acc + next, 0);
   // console.log(storage.map((check) => check.item).reduce((acc, next) => acc + next, 0));
 };
 
-// still display amount on cart icon when page updated
+// still show amount on cart icon when page updated
 updateCal();
